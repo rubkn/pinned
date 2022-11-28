5c2e5ca81f08e05911b6fa47072a423871a6e062
rubenbrandao <rubenavbrandao@gmail.com>
2022-11-28 02:19:26 +0000

    init with nextjs

---
+++ graphql/user-client.ts
@@ -0,0 +1,22 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `bearer ${process.env.GH_TOKEN}`,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
---
+++ graphql/user.ts
@@ -0,0 +1,52 @@
+import { gql } from "@apollo/client";
+
+import userClient from "./user-client";
+
+const GET_PINNED_REPOS = gql`
+  query user($username: String!) {
+    user(login: $username) {
+      name
+      company
+      bio
+      twitterUsername
+      websiteUrl
+      url
+      pinnedItems(first: 6) {
+        totalCount
+        edges {
+          node {
+            ... on Repository {
+              id
+              name
+              description
+              url
+              stargazerCount
+              forkCount
+              languages(first: 3) {
+                edges {
+                  node {
+                    name
+                    color
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const getPinnedRepos = async (username: string) => {
+  console.log("username", username);
+
+  const {
+    data: { user },
+  } = await userClient.query({
+    query: GET_PINNED_REPOS,
+    variables: { username },
+  });
+
+  return { user };
+};
