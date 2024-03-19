30c5ff581cc9d61a54128f24f0984942c3b48a7c
rubkn <rubenavbrandao@gmail.com>
2024-03-19 11:29:51 +0000

    prettier and new icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,9 +23,9 @@ export default function Home() {
       <main className="font-sans p-6 sm:p-12">
         <section className="text-lg text-neutral-200 ">
           <h1 className="text-5xl font-bold font-mono">pinned</h1>
-          <h1 className="py-2 text-base">
+          <h2 className="py-2 text-base">
             Your pinned github repositories in an instant!
-          </h1>
+          </h2>
 
           <hr className="my-8 h-px bg-gray-200 border-0 dark:bg-neutral-800" />
           <div>
@@ -34,9 +34,8 @@ export default function Home() {
               What is <span className="font-bold font-mono">pinned</span>?
             </h2>
             <p className="py-4">
-              <span className="font-bold font-mono">pinned</span> is a simple
-              API service that allows you to get the pinned repositories of any
-              github user.
+              <span className="font-bold font-mono">pinned</span> is a simple API service
+              that allows you to get the pinned repositories of any github user.
             </p>
 
             {/* input and button */}
@@ -60,10 +59,10 @@ export default function Home() {
               How to use <span className="font-bold font-mono">pinned</span>?
             </h2>
             <p className="py-2">
-              To use <span className="font-bold font-mono">pinned</span>, you
-              just need to make a GET request to{" "}
-              <span className="font-bold font-mono">pinned</span> API with the
-              username of the github user you want to get the pinned repos from.
+              To use <span className="font-bold font-mono">pinned</span>, you just need to
+              make a GET request to <span className="font-bold font-mono">pinned</span>{" "}
+              API with the username of the github user you want to get the pinned repos
+              from.
             </p>
             <p className="py-2">
               The API will return a JSON with the pinned repos of the user.
@@ -79,23 +78,17 @@ export default function Home() {
             </p>
             <p className="py-2">
               For example, to get the{" "}
-              <a
-                href="//vercel.com"
-                className="text-blue-600 hover:text-blue-800"
-              >
+              <a href="//vercel.com" className="text-blue-600 hover:text-blue-800">
                 Vercel
               </a>{" "}
               pinned repositories, you just need to make a GET request to{" "}
-              <Link
-                href="/api/user/vercel"
-                className="text-blue-600 hover:text-blue-800"
-              >
+              <Link href="/api/user/vercel" className="text-blue-600 hover:text-blue-800">
                 /api/user/vercel
               </Link>
             </p>
             <p className="py-4 text-xs text-neutral-500">
-              Note: This app is still in development, features may be added or
-              removed in the future.
+              Note: This app is still in development, features may be added or removed in
+              the future.
             </p>
           </div>
         </section>
@@ -105,10 +98,7 @@ export default function Home() {
         <footer className="flex flex-col">
           <span>
             Developed by{" "}
-            <a
-              href="//rubenbrandao.com"
-              className="font-bold hover:text-blue-600"
-            >
+            <a href="//rubenbrandao.com" className="font-bold hover:text-blue-600">
               rubkn
             </a>
           </span>
